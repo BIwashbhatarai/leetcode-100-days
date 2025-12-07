c3795340ed048ff76c82075c684d6e1d80d12a8d
Biwash Bhattarai <biwashbhattarai5@gmail.com>
2025-12-07 08:25:09 +0545

    chore: format Day 2 Plus One solution for GitHub

--- Day02_PlusOne.py
+++ Day02_PlusOne.py
@@ -1,8 +1,20 @@
+# LeetCode 66: Plus One
+# Convert digits to integer, add one, convert back to list
+
 class Solution(object):
     def plusOne(self, digits):
+        # Convert list of digits to integer
         n = int(''.join(map(str, digits)))
+        
+        # Add one
         n += 1
+        
+        # Convert back to list of digits
         arr = [int(d) for d in str(n)]
         return arr
-obj = Solution()
-print(obj.plusOne([1,2,3]))
+
+
+# Example run
+if __name__ == "__main__":
+    obj = Solution()
+    print(obj.plusOne([1, 2, 3]))  # Output: [1, 2, 4]
